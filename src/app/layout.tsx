45fdef74dedc7d5c021ef2425e462eac2d7fd226
ViktorM <viktor@malmedal.dev>
2023-02-09 16:16:39 +0100

    layout styling

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,13 +4,15 @@ import "./globals.css";
 import { Lora, Lato } from "@next/font/google";
 
 const lora = Lora({
-  variable: "--lora",
+  variable: "--font-lora",
   display: "swap",
+  subsets: ["latin"],
 });
 
 const lato = Lato({
   weight: ["400", "700"],
-  variable: "--lato",
+  subsets: ["latin"],
+  variable: "--font-lato",
   display: "swap",
 });
 
@@ -40,12 +42,17 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html
       lang="en"
-      className={clsx("bg-neutral-100 text-black dark:bg-[#121018] dark:text-white", lora.variable, lato.variable)}
+      className={clsx(
+        "h-full bg-neutral-100 text-black",
+        "dark:bg-gradient-to-t dark:from-[#06040F] dark:to-[#14121B] dark:text-white",
+        lora.variable,
+        lato.variable,
+      )}
     >
       <head />
-      <body className="relative mx-4 flex h-screen max-w-screen-lg flex-row antialiased md:flex-col lg:mx-auto">
+      <body className="relative mx-4 mt-28 flex max-w-screen-md antialiased lg:mx-auto">
         <SideNavigation />
-        <main className="flex min-w-0 flex-auto flex-col px-2 pt-24 md:mt-0 md:px-0">{children}</main>
+        <main className="flex min-w-0 flex-auto flex-col px-2 md:mt-0 md:px-0">{children}</main>
       </body>
     </html>
   );
