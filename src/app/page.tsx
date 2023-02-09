98112f1709bdd0593ad9fd1e88ac4b5d2927099e
ViktorM <viktor@malmedal.dev>
2023-02-09 16:16:47 +0100

    wip frontpage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,30 @@
-import { GithubIcon, LinkedInIcon, MailIcon } from "@/components/icons";
+import { GithubIcon, LinkedInIcon, MailIcon, VimaIcon } from "@/components/icons";
 import Image from "next/image";
 
 export default async function HomePage() {
   return (
-    <div className="flex flex-row justify-between">
-      <h1 className="relative text-4xl">Viktor Malmedal</h1>
-      <div className="flex flex-col items-center gap-2">
-        <Image className="rounded-full" src="/images/viktor.jpg" alt="profile image" width={100} height={100} />
-        <div className="flex gap-2">
-          <GithubIcon />
-          <LinkedInIcon />
-          <MailIcon />
+    <>
+      <div className="flex justify-between">
+        <div>
+          <div className="flex items-center gap-4">
+            <VimaIcon />
+            <h1 className="relative font-serif text-4xl font-semibold">Viktor Malmedal</h1>
+          </div>
+          <p>abuot me </p>
+        </div>
+        <div className="flex flex-col items-center gap-4">
+          <Image className="rounded-full" src="/images/viktor.jpg" alt="profile image" width={100} height={100} />
+          <div className="flex gap-4">
+            <GithubIcon />
+            <LinkedInIcon />
+            <MailIcon />
+          </div>
         </div>
       </div>
-    </div>
+      <button className="inline-flex items-center gap-2 text-sm">
+        <GithubIcon />
+        <span className="opacity-80 hover:opacity-100">View source</span>
+      </button>
+    </>
   );
 }
