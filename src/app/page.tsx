e6e40b676d0ea794e45019f453a5cb1e7132d43d
ViktorM <viktor@malmedal.dev>
2023-02-09 10:47:31 +0100

    boilerplate page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,18 @@
-export default function Home() {
-  return <h1>hello world</h1>;
+import { GithubIcon, LinkedInIcon, MailIcon } from "@/components/icons";
+import Image from "next/image";
+
+export default async function HomePage() {
+  return (
+    <div className="flex flex-row justify-between">
+      <h1 className="relative text-4xl">Viktor Malmedal</h1>
+      <div className="flex flex-col items-center gap-2">
+        <Image className="rounded-full" src="/images/viktor.jpg" alt="profile image" width={100} height={100} />
+        <div className="flex gap-2">
+          <GithubIcon />
+          <LinkedInIcon />
+          <MailIcon />
+        </div>
+      </div>
+    </div>
+  );
 }
