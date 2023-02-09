e2922b0f024ab218b6cf2a4bf098af7876945c65
ViktorM <viktor@malmedal.dev>
2023-02-09 10:47:25 +0100

    add fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,18 @@
-import { BottomNavigation } from "@/components/bottom-navigation";
+import { SideNavigation } from "@/components/side-navigation";
+import { clsx } from "clsx";
 import "./globals.css";
+import { Lora, Lato } from "@next/font/google";
+
+const lora = Lora({
+  variable: "--lora",
+  display: "swap",
+});
+
+const lato = Lato({
+  weight: ["400", "700"],
+  variable: "--lato",
+  display: "swap",
+});
 
 export const metadata = {
   title: {
@@ -25,11 +38,14 @@ export const metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en" className="bg-neutral-100 text-black dark:bg-[#121018] dark:text-white">
+    <html
+      lang="en"
+      className={clsx("bg-neutral-100 text-black dark:bg-[#121018] dark:text-white", lora.variable, lato.variable)}
+    >
       <head />
       <body className="relative mx-4 flex h-screen max-w-screen-lg flex-row antialiased md:flex-col lg:mx-auto">
+        <SideNavigation />
         <main className="flex min-w-0 flex-auto flex-col px-2 pt-24 md:mt-0 md:px-0">{children}</main>
-        <BottomNavigation />
       </body>
     </html>
   );
