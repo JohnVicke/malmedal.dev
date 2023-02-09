dfa8479cc232cd7fc6ce34494f801b414210c2f2
ViktorM <viktor@malmedal.dev>
2023-02-09 16:16:55 +0100

    sidenav

--- src/components/side-navigation.tsx
+++ src/components/side-navigation.tsx
@@ -1,5 +1,48 @@
 "use-client";
 
+import Link from "next/link";
+
+const navigationItems = [
+  {
+    title: "Home",
+    href: "/",
+  },
+  {
+    title: "About",
+    href: "/about",
+  },
+  {
+    title: "Blog",
+    href: "/blog",
+  },
+  {
+    title: "Projects",
+    href: "/projects",
+  },
+  {
+    title: "Tools",
+    href: "/tools",
+  },
+  {
+    title: "Guestbook",
+    href: "/guest-book",
+  },
+];
+
 export const SideNavigation = () => {
-  return <aside className="fixed bottom-4 left-1/2 mx-auto -translate-x-1/2">bototm nav</aside>;
+  return (
+    <aside className="sticky min-w-[200px]">
+      <nav className="flex flex-col gap-2">
+        {navigationItems.map(({ href, title }) => (
+          <Link
+            href={href}
+            key={href}
+            className="block w-fit rounded-md p-2 text-sm font-semibold hover:bg-neutral-200 dark:hover:bg-neutral-800"
+          >
+            {title}
+          </Link>
+        ))}
+      </nav>
+    </aside>
+  );
 };
