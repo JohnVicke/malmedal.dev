785de7ebe31c3e2f2e5720914d9d807e9d36b78f
ViktorM <viktor@malmedal.dev>
2023-02-11 17:32:17 +0100

    update side navigation

--- src/components/side-navigation.tsx
+++ src/components/side-navigation.tsx
@@ -1,6 +1,9 @@
-"use-client";
+"use client";
+
+import { usePathname } from "next/navigation";
 
 import Link from "next/link";
+import clsx from "clsx";
 
 const navigationItems = [
   {
@@ -25,23 +28,30 @@ const navigationItems = [
   },
   {
     title: "Guestbook",
-    href: "/guest-book",
+    href: "/guestbook",
   },
 ];
 
 export const SideNavigation = () => {
+  const pathname = usePathname();
   return (
     <aside className="sticky min-w-[200px]">
       <nav className="flex flex-col gap-2">
-        {navigationItems.map(({ href, title }) => (
-          <Link
-            href={href}
-            key={href}
-            className="block w-fit rounded-md p-2 text-sm font-semibold hover:bg-neutral-200 dark:hover:bg-neutral-800"
-          >
-            {title}
-          </Link>
-        ))}
+        {navigationItems.map(({ href, title }) => {
+          const isActive = pathname === href;
+          return (
+            <Link
+              href={href}
+              key={href}
+              className={clsx(
+                "block w-fit rounded-md p-2 font-serif text-sm font-normal opacity-70 hover:bg-neutral-200 dark:hover:bg-neutral-800",
+                isActive && "bg-neutral-200 font-bold opacity-100 dark:bg-neutral-800",
+              )}
+            >
+              {title}
+            </Link>
+          );
+        })}
       </nav>
     </aside>
   );
