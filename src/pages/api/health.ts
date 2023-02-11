4647c2f320065ad5080c2a42748bb26c08110fe8
ViktorM <viktor@malmedal.dev>
2023-02-11 17:32:23 +0100

    update return shape

--- src/pages/api/health.ts
+++ src/pages/api/health.ts
@@ -10,16 +10,12 @@ type UpTimeMessage = {
   status: string;
 };
 
-type Data<T> =
-  | {
-      data: T;
-    }
-  | { error: ErrorMessage[] };
+type Data<T> = T | { error: ErrorMessage[] };
 
 export default function handler(req: NextApiRequest, res: NextApiResponse<Data<UpTimeMessage>>) {
   if (req.method !== "GET") {
     res.status(405).json({ error: [{ message: "Method not allowed" }] });
     return;
   }
-  res.status(200).json({ data: { message: "Hello there!", status: "up", version: "v1" } });
+  res.status(200).json({ message: "Hello there!", status: "up", version: "v1" });
 }
