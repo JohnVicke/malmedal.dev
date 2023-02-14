39e5e985b6a0379f77778b2b5993c3e846670830
Viktor Malmedal <viktor@malmedal.dev>
2023-02-14 18:42:57 +0100

    use prisma adapter

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,11 +1,33 @@
 import NextAuth, { NextAuthOptions } from "next-auth";
-import GithubProvider from "next-auth/providers/github";
+import GithubProvider, { GithubProfile } from "next-auth/providers/github";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { prisma } from "@/lib/prisma";
+import { User } from "@prisma/client";
 
 export const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(prisma),
+  secret: process.env.NEXTAUTH_SECRET as string,
+  callbacks: {
+    session({ session, user }) {
+      if (user) {
+        session.user.id = user.id;
+      }
+      return session;
+    },
+  },
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_CLIENT_ID as string,
       clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
+      profile: (profile: GithubProfile): User => {
+        return {
+          name: profile.name,
+          email: profile.email,
+          id: profile.id.toString(),
+          image: profile.image,
+          htmlUrl: profile.html_url,
+        };
+      },
     }),
   ],
 };
