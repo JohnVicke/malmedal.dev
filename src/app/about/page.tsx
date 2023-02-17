48fa97309d787fd4b7f174537318cf7145078123
Viktor Malmedal <viktor@malmedal.dev>
2023-02-17 16:09:42 +0100

    add placeholder

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,5 +1,12 @@
 import { Heading } from "@/components/heading";
 
 export default function About() {
-  return <Heading>About me</Heading>;
+  return (
+    <>
+      <Heading>About me</Heading>
+      <p className="font-semibold">
+        TL;DR <span className="font-normal italic opacity-80">I Like to create cool stuff</span>
+      </p>
+    </>
+  );
 }
