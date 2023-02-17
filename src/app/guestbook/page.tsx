33d7c53fcdea3498bbdefe6fdb0e39ae5199e37e
ViktorM <viktor@malmedal.dev>
2023-02-17 09:44:28 +0100

    fetch actual data

--- src/app/guestbook/page.tsx
+++ src/app/guestbook/page.tsx
@@ -8,8 +8,12 @@ import { SignGuestbookForm } from "./sign-guestbook-form";
 import { SignInButton } from "./sign-in-button";
 import { SignOutButton } from "./sign-out-button";
 
+function fetchMessages() {
+  return fetchApi<PostWithAuthor[]>("/guestbook");
+}
 
 export default async function Guestbook() {
+  const { data: messages, error } = await fetchMessages();
   const session = await getServerSession(authOptions);
 
   return (
@@ -17,16 +21,20 @@ export default async function Guestbook() {
       {!session?.user ? <SignInButton /> : <SignOutButton />}
       <SignGuestbookForm />
       <ul className="flex flex-col gap-4">
-        {mockMessage.map(({ id, message, name, updateAt }) => (
+        {messages?.map(({ id, updatedAt, content, author }) => (
           <li className="rounded-md px-4 py-2 hover:bg-[#0B0713]" key={id}>
             <div className="flex justify-between opacity-60 ">
-              <h3 className="flex items-center gap-2">
-                <GithubIcon />
-                {name}
-              </h3>
-              <h4>{updateAt.toLocaleString()}</h4>
+              {author.htmlUrl && (
+                <CustomLink href={author.htmlUrl}>
+                  <h3 className="flex items-center gap-2">
+                    <GithubIcon />
+                    {author?.name}
+                  </h3>
+                </CustomLink>
+              )}
+              <h4>{new Date(updatedAt).toLocaleDateString()}</h4>
             </div>
-            <p>{message}</p>
+            <p>{content}</p>
           </li>
         ))}
       </ul>
