c2189bf9d7195bb79b85464bb02431220efc81aa
Viktor Malmedal <viktor@malmedal.dev>
2023-02-17 16:10:11 +0100

    update signin button style

--- src/app/guestbook/sign-in-button.tsx
+++ src/app/guestbook/sign-in-button.tsx
@@ -1,7 +1,15 @@
 "use client";
 
+import { GithubIcon } from "@/components/icons";
 import { signIn } from "next-auth/react";
 
 export const SignInButton = () => {
-  return <button onClick={() => signIn()}>Continue using GitHub</button>;
+  return (
+    <button
+      onClick={() => signIn()}
+      className="inline-flex w-fit items-center gap-4 rounded-md border border-gray-200 px-4 py-2 font-sans"
+    >
+      <GithubIcon /> Continue using GitHub
+    </button>
+  );
 };
