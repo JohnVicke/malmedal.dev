45c1677d4b7abbdc381963b24d3e4c1c1ed99018
Viktor Malmedal <viktor@malmedal.dev>
2023-02-17 13:53:05 +0100

    add image to type

--- src/types/post-with-author.ts
+++ src/types/post-with-author.ts
@@ -1,3 +1,5 @@
 import { Post, User } from "@prisma/client";
 
-export type PostWithAuthor = Omit<Post, "createdAt" | "authorId"> & { author: Pick<User, "name" | "htmlUrl"> };
+export type PostWithAuthor = Omit<Post, "createdAt" | "authorId"> & {
+  author: Pick<User, "name" | "htmlUrl" | "image">;
+};
