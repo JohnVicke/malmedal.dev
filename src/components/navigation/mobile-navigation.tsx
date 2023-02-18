9ba8fe8639efde49605325d78cfd75b3aa553919
Viktor Malmedal <viktor@malmedal.dev>
2023-02-18 18:21:20 +0100

    fix jsx errors

--- src/components/navigation/mobile-navigation.tsx
+++ src/components/navigation/mobile-navigation.tsx
@@ -21,11 +21,11 @@ export const MobileNavigation = ({ className }: { className?: string }) => {
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
           viewBox="0 0 24 24"
-          stroke-width="1.5"
+          strokeWidth="1.5"
           stroke="currentColor"
           className="h-6 w-6"
         >
-          <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 15.75l7.5-7.5 7.5 7.5" />
+          <path strokeLinecap="round" strokeLinejoin="round" d="M4.5 15.75l7.5-7.5 7.5 7.5" />
         </svg>
       </button>
       {open && (
