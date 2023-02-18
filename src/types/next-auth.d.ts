25077d88a5563e63f5d179ce2c9361ba516dd47d
Viktor Malmedal <viktor@malmedal.dev>
2023-02-18 18:21:32 +0100

    override defaultuser type

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -1,11 +1,11 @@
-import { Profile, DefaultUser } from "next-auth";
-import { GithubProfile } from "next-auth/providers/github";
+import NextAuth, { DefaultUser, User } from "next-auth";
 import { JWT } from "next-auth/jwt";
 
 declare module "next-auth" {
+  interface DefaultUser {
+    role: "ADMIN" | "USER";
+  }
   interface Session {
-    user: DefaultUser & {
-      id: string;
-    };
+    user: DefaultUser;
   }
 }
