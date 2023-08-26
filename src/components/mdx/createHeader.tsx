1fb7decc9cb76f4043a72240afa3dc45a0796d43
Viktor Malmedal <viktor@malmedal.dev>
2023-08-26 22:02:21 +0200

    💄 add margin to mdx header

--- src/components/mdx/createHeader.tsx
+++ src/components/mdx/createHeader.tsx
@@ -4,6 +4,6 @@ import Text, { type TextComponent, type TextProps } from "../Text";
 
 export const createHeader = (level: Exclude<TextComponent, "p">) => {
   return (props: Omit<TextProps<TextComponent>, "level">) => (
-    <Text {...props} level={level} />
+    <Text className="my-4" {...props} level={level} />
   );
 };
