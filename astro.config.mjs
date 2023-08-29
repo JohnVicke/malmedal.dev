0f636111eb84a8dfb411d24678e552fc03d6144c
Viktor Malmedal <viktor@malmedal.dev>
2023-08-29 12:12:58 +0200

    ♻️ move estimated read to mdx plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,43 @@ import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/edge";
 import { defineConfig } from "astro/config";
+import { toString as mdastToString } from "mdast-util-to-string";
 import rehypePrettyCode from "rehype-pretty-code";
+
+/**
+ * @param {string} content
+ * @param {number} wpm
+ * @returns {number}
+ * */
+function estimateReadingTime(content, wpm = 200) {
+  const lines = content.split("\n");
+  const startIndex = lines.findIndex((line) => line.trim().startsWith("#"));
+  const cleanedContent = lines.slice(startIndex).join("\n");
+  const wordCount = cleanedContent
+    .split(/\s+/)
+    .filter((word) => word.length > 0).length;
+  return Math.ceil(wordCount / wpm);
+}
+
+/**
+ * @typedef {import('mdast').Root} Root
+ *
+ * @typedef Options
+ *   Configuration (optional).
+ * @property {Number} [wpm]
+ *   Word per minute (default: 200).
+ */
+
+/** @type {import('unified').Plugin<[Options?], Root>} */
+function remarkReadingTime(options) {
+  return function transformer(tree, { data }) {
+    const textOnPage = mdastToString(tree);
+    data.astro.frontmatter.minutesRead = estimateReadingTime(
+      textOnPage,
+      options?.wpm,
+    );
+  };
+}
 
 /** @type {import('rehype-pretty-code').Options} */
 const prettyCodeOptions = {
@@ -31,7 +67,6 @@ const prettyCodeOptions = {
   tokensMap: {},
 };
 
-// https://astro.build/config
 export default defineConfig({
   integrations: [
     tailwind({
@@ -44,6 +79,7 @@ export default defineConfig({
   markdown: {
     syntaxHighlight: false,
     rehypePlugins: [[rehypePrettyCode, prettyCodeOptions]],
+    remarkPlugins: [remarkReadingTime],
   },
   vite: {
     ssr: {
